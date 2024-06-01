60b65dd347959412d9d16e8a664c94a65dfb3834
Aegisystem <steven14correa@gmail.com>
2024-06-01 11:33:30 -0500

    Template schema complete

--- models/templates.js
+++ models/templates.js
@@ -2,33 +2,45 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Define the schema for the fields array
-const FieldSchema = new Schema({
-  name: { 
-    type: String,
-    unique: true, 
-    required: true 
-},
-  datatype: { 
-    type: String, 
-    required: true 
-},
-  required: {
-    type: Boolean,
-    required: true
-},
-  validate_with: {
-    type: String,
-    required: false} // Reference to another collection for validation
+const fieldSchema = new Schema({
+    name: { 
+        type: String,
+        unique: true, 
+        required: true 
+    },
+    datatype: { 
+        type: String, 
+        required: true 
+    },
+    required: {
+        type: Boolean,
+        required: true
+    },
+    validate_with: {
+        type: String,
+        required: false
+    } // Reference to another collection for validation
 });
 
 // Define the schema for the main template
-const TemplateSchema = new Schema({
-  name: { type: String, required: true },
-  file_name: { type: String, required: true },
-  file_description: { type: String, required: true },
-  fields: [FieldSchema] // Array of fields
-}, { collection: 'Plantillas' }); // Name of the collection in the database
+const templateSchema = new Schema({
+    name: { 
+        type: String, required: true 
+    },
+    file_name: {
+        type: String,
+        required: true
+    },
+    file_description: { 
+        type: String,
+        required: true
+    },
+    fields: [fieldSchema] // Array of fields
+}, 
+{
+    versionKey: false,
+    timestamps: true
+}
+); // Name of the collection in the database
 
-const Plantilla = mongoose.model('Plantilla', TemplateSchema);
-
-module.exports = Plantilla;
+module.exports = mongoose.model('templates', templateSchema);;
