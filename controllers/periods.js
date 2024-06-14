4b289dc56e2e58a8feefcda06331e2961ea0c797
Brayan Patiño <brayandavid1928@gmail.com>
2024-06-14 12:18:59 -0500

    Add search in controller

--- controllers/periods.js
+++ controllers/periods.js
@@ -3,20 +3,49 @@ const Period = require('../models/periods');
 const periodController = {};
 
 periodController.getPeriods = async (req, res) => {
-    const periods = await Period.find();
-    res.status(200).json(periods);
-}
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const search = req.query.search || '';
+    const skip = (page - 1) * limit;
+
+    try {
+        const query = {};
+
+        if (search) {
+            query.$or = [
+                { name: { $regex: search, $options: 'i' } }
+            ];
+
+            if (search.toLowerCase() === 'activo' || search.toLowerCase() === 'inactivo') {
+                query.$or.push({ is_active: search.toLowerCase() === 'activo' });
+            }
+        }
+
+        const periods = await Period.find(query).skip(skip).limit(limit);
+        const total = await Period.countDocuments(query);
+
+        res.status(200).json({
+            periods,
+            total,
+            page,
+            pages: Math.ceil(total / limit)
+        });
+    } catch (error) {
+        console.error('Error fetching periods:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
 
 periodController.getPeriod = async (req, res) => {
     const start_date = req.body.start_date;
-    const period = await Period.findOne({start_date});
+    const period = await Period.findOne({ start_date });
     res.status(200).json(period);
 }
 
 periodController.createPeriod = async (req, res) => {
-    const period = new Period( req.body );
+    const period = new Period(req.body);
     await period.save();
-    res.status(200).json({status: "Period created"});
+    res.status(200).json({ status: "Period created" });
 }
 
 periodController.updatePeriod = async (req, res) => {
@@ -45,4 +74,4 @@ periodController.deletePeriod = async (req, res) => {
     }
 }
 
-module.exports = periodController
+module.exports = periodController;
