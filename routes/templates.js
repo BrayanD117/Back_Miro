909035a1363976b805ca7f3822e507d1f4a36e9f
Brayan Patiño <brayandavid1928@gmail.com>
2024-06-23 12:04:12 -0500

    Fix templates' routes

--- routes/templates.js
+++ routes/templates.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const controller = require('../controllers/templates'); // Ajusta el path según tu estructura de proyecto
 
+router.get("/creator", controller.getPlantillasByCreator); // Obtener plantillas por creador
 router.get("/all", controller.getPlantillas);       // Obtener todas las plantillas
 router.get("/:id", controller.getPlantilla);        // Obtener una plantilla por ID
-router.get("/creator", controller.getPlantillasByCreator); // Obtener plantillas por creador
 router.post("/create", controller.createPlantilla);  // Crear una nueva plantilla
 router.put("/:id", controller.updatePlantilla);
 router.delete("/delete", controller.deletePlantilla); // Borrar una plantilla por ID
