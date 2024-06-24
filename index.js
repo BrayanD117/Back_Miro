99406b87a77d4ba7faab506804cbdd1b131b3623
Brayan Patiño <brayandavid1928@gmail.com>
2024-06-24 13:30:02 -0500

    Update cors and CI/CD

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ const app = express()
 
 require('dotenv').config()
 
+const allowedOrigins = ['http://localhost:3000', 'http://miro.unibague.edu.co'];
+
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: function(origin, callback){
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
+
 app.use(express.json() )
 
 app.use(express.urlencoded({
