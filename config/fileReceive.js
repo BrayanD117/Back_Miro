99249d6a09c8391ab9ed41e2660c6ed68ba66de0
Steven Correa <steven14correa@gmail.com>
2024-09-03 12:26:57 -0500

    Last test for saving in route

--- config/fileReceive.js
+++ config/fileReceive.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fileUpload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, '/uploads');
+      cb(null, '/home/miro/uploads');
     },
     filename: (req, file, cb) => {
       cb(null, file.originalname);
