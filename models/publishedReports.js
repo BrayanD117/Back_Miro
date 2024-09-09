8dba992628f27e03bd85436cb908990e389c48eb
Steven Correa <steven14correa@gmail.com>
2024-09-09 15:53:56 -0500

    Report drafts almost done

--- models/publishedReports.js
+++ models/publishedReports.js
@@ -1,107 +1,117 @@
-const mongoose = require('mongoose');
-const { validate } = require('./reports');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const driveFile = new Schema({
+const driveFile = new Schema(
+  {
     id: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     view_link: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     download_link: {
-        type: String,
-        required: true
-    },
-    folder_id: {
-        type: String,
-        required: true
-    }
-}, {
-    _id: false,
-    versionKey: false
-});
-
-const filledReportSchema = new Schema({
-    dimension: {
-        type: Schema.Types.ObjectId,
-        ref: 'dimensions',
-        required: true
-    },
-    send_by: {
-        type: {},
-        required: true
-    },
-    loaded_date: {
-        type: Date,
-        required: true,
-    },
-    report_file: {
-        type: driveFile,
-        required: true
-    },
-    attachments: {
-        type: [driveFile]
-    },
-    status: {
-        type: String,
-        enum: ['En Borrador', 'En Revisión', 'Aprobado', 'Rechazado'],
-        default: 'En Borrador'
+      type: String,
+      required: true,
     },
     folder_id: {
       type: String,
-      required: true
+      required: true,
+    },
+    comments: {
+      type: String,
+    },
+  },
+  {
+    _id: false,
+    versionKey: false,
+  }
+);
+
+const filledReportSchema = new Schema(
+  {
+    dimension: {
+      type: Schema.Types.ObjectId,
+      ref: "dimensions",
+      required: true,
+    },
+    send_by: {
+      type: {},
+      required: true,
+    },
+    loaded_date: {
+      type: Date,
+      required: true,
+    },
+    report_file: {
+      type: driveFile,
+      required: true,
+    },
+    attachments: {
+      type: [driveFile],
+    },
+    status: {
+      type: String,
+      enum: ["En Borrador", "En Revisión", "Aprobado", "Rechazado"],
+      default: "En Borrador",
+    },
+    folder_id: {
+      type: String,
+      required: true,
     },
     //TODO Improve for required
     status_date: {
       type: Date,
-      required: true
+      required: true,
     },
     observations: {
-        type: String
-    }
-}, {
+      type: String,
+    },
+  },
+  {
     versionKey: false,
-    timestamps: true
-});
+    timestamps: true,
+  }
+);
 
-const publishedReportSchema = new Schema({
+const publishedReportSchema = new Schema(
+  {
     report: {
-        type: {},
-        required: true
+      type: {},
+      required: true,
     },
     period: {
-        type: Schema.Types.ObjectId,
-        ref: 'periods',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "periods",
+      required: true,
     },
     dimensions: {
-        type: [Schema.Types.ObjectId],
-        ref: 'dimensions',
-        validate: [
-            {
-                validator: function (v) {
-                    return v.length > 0;
-                },
-                message: 'At least one dimension is required'
-            }
-        ],
-        required: true
+      type: [Schema.Types.ObjectId],
+      ref: "dimensions",
+      validate: [
+        {
+          validator: function (v) {
+            return v.length > 0;
+          },
+          message: "At least one dimension is required",
+        },
+      ],
+      required: true,
     },
     filled_reports: {
-        type: [filledReportSchema]
+      type: [filledReportSchema],
     },
-    folder_id: String
-
-}, {
+    folder_id: String,
+  },
+  {
     versionKey: false,
-    timestamps: true
-})
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('publishedReports', publishedReportSchema)
+module.exports = mongoose.model("publishedReports", publishedReportSchema);
