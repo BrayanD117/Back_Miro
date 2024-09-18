b03c2ebe435d6c5d5289f68fe67a67d4d7c42bb0
Steven Correa <steven14correa@gmail.com>
2024-09-18 11:49:27 -0500

    Logs model created

---
+++ models/logs.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const User = require("./users");
+
+const ErrorDescription = new Schema({
+  register: {
+    type: Number,
+    required: true,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  }
+}, {
+  _id: false
+});
+
+const Error = new Schema({
+  column: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: ErrorDescription,
+    required: true,
+  },
+}, {
+  _id: false
+});
+
+const logSchema = new mongoose.Schema(
+  {
+    user: {
+      type: User.schema,
+      required: true,
+    },
+    published_template: {
+      type: Schema.Types.ObjectId,
+      ref: "publishedTemplates",
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true
+    },
+    errors: {
+      type: Error,
+      required: true
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
